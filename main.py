31e056e740edbcc6a90abe380c66493e5274ea9d
Ajinkyaatgeir9834 <ajinkya.gaidhani@atgeirsolutions.com>
2026-04-07 11:57:13 +0530

    Initial commit1

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+from google.cloud import storage
+from google.cloud import bigquery
+import pandas as pd
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello I am Ajinkya from Cloud Run 🚀"
+@app.route("/", methods=["POST"])
+def process_file():
+    data = request.get_json()
+
+    bucket_name = data['bucket']
+    file_name = data['name']
+
+    # Read from GCS
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    file_path = f"/tmp/{file_name}"
+    blob.download_to_filename(file_path)
+
+    # Read CSV
+    df = pd.read_csv(file_path)
+
+    # Simple cleaning
+    df = df.dropna()
+
+    # Load to BigQuery
+    bq_client = bigquery.Client()
+    table_id = "your_project.your_dataset.demo_table"
+
+    job = bq_client.load_table_from_dataframe(df, table_id)
+    job.result()
+
+    return "Loaded successfully", 200
